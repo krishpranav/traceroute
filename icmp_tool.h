81ac0ce42523ff3b37d7139a535271157e6b63f1
krishpranav <krisna.pranav@gmail.com>
2021-06-04 11:24:00 +0530

    [+] send simple icmp [+]

--- icmp_tool.h
+++ icmp_tool.h
@@ -5,3 +5,5 @@
 
 #define handle_error(msg) do { perror(msg " error"); exit(EXIT_FAILURE); \
                           } while (0)
+
+void send_single_icmp(int sockfd, const char *ip, uint16_t id, uint16_t sequence, int ttl);
