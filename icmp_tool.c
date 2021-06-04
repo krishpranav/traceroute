027f36fb9e7de91831687d5c2f0e269c356233d3
krishpranav <krisna.pranav@gmail.com>
2021-06-04 11:27:12 +0530

    [+] compute_icmp_checksum update [+]

--- icmp_tool.c
+++ icmp_tool.c
@@ -10,3 +10,13 @@
 #include <sys/select.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+
+static uint16_t compute_icmp_checksum(const void *buff, int length) {
+    uint32_t sum;
+    const uint16_t *ptr = buff;
+    assert (length % 2 == 0);
+    for (sum = 0; length > 0; length -= 2)
+        sum += *ptr++;
+    sum = (sum >> 16) + (sum & 0xffff);
+    return (uint16_t)(~(sum + (sum >> 16)));
+}
