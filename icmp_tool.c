912b736b3f97b45f046ff6fe40d9ab77a4ca2866
krishpranav <krisna.pranav@gmail.com>
2021-06-04 11:25:02 +0530

    [+] includes [+]

---
+++ icmp_tool.c
@@ -0,0 +1,12 @@
+// includes
+#include "icmp_tools.h"
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
